aa0efe3d4b8f5dbadf3fbdbe3eca8823013fb6be
F5XS <f5xs.0000a@gmail.com>
2023-06-08 01:52:39 +0800

    2023-06-07 file

--- scripts/hack_a_machine.js
+++ scripts/hack_a_machine.js
@@ -51,12 +51,50 @@ export async function main(ns) {
     disable_logs(ns);
     let target = ns.args[0];
 
-    let hack_stats = HackStatistics();
+    //let hack_stats = HackStatistics();
+
+    let hack_successes = 0;
+    let hack_attempts = 0;
+
+    // start at whatever current optimal hack rate there is. $10/second?
+    let hack_rate = 1;
+    let hack_accel = 0;
 
     while (true) {
+        ns.print("\n" + "=" * 80);
+
         if (hack_accel < 0) {
+            ns.print("Growing...");
             await ns.grow(target);
+            ns.print("\n" + "=" * 80);
         }
+
+        // hacking statistics
+        let hack_start = Date.now();
+        let hacked_amount = await ns.hack(target);
+        let hack_duration = (Date.now() - hack_start) / 1_000;
+
+        hack_attempts += 1;
+        
+        // if we failed to hack
+        if (hacked_amount == 0) {
+            ns.print("Weakening...");
+            await ns.weaken(target);
+            continue;
+        }
+        
+        let new_hack_rate = hacked_amount / hack_duration;
+        let new_hack_accel = new_hack_rate - hack_rate;
+
+        hack_rate = new_hack_rate;
+        hack_accel = new_hack_accel;
+
+        hack_successes += 1;
+
+        ns.print("Hack Duration:     " + hack_duration + "s");
+        ns.print("Hacked Amount:     $" + hacked_amount);
+        ns.print("Hack Rate:         " + new_hack_rate);
+        ns.print("Hack Acceleration: " + new_hack_accel);
 
         //await hack_stats.attempt(ns);
 
