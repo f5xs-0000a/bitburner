91a4a4e136e5721446a7d94c43c443679991570c
F5XS <f5xs.0000a@gmail.com>
2023-06-08 02:28:26 +0800

    added nukebot

--- contracts/total_ways_to_sum_2.rs
+++ contracts/total_ways_to_sum_2.rs
@@ -12,6 +12,7 @@
 
 use std::io::BufRead as _;
 use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::collections::HashSet;
 
 fn get_a_number(prompt: &str) -> i64 {
@@ -51,31 +52,42 @@ fn main() {
     let mut known_solutions = HashMap::new();
     let solutions = solve(sum, &values, &mut known_solutions);
 
-    eprintln!("Solutions: {}", solutions);
+    dbg!(&known_solutions);
+
+    eprintln!("Solutions: {}", known_solutions[&sum].len());
 }
 
 fn solve(
     sum: i64,
     values: &HashSet<i64>,
-    known_solutions: &mut HashMap<i64, usize>
-) -> usize {
+    known_solutions: &mut HashMap<i64, HashSet<BTreeMap<i64, usize>>>,
+) {
     if let Some(solutions) = known_solutions.get(&sum) {
-        return *solutions;
+        return; // just ask the caller to find it inside known_solutions
     }
 
-    let mut solutions = 0;
+    if values.contains(&sum) {
+        let mut sole_solution = BTreeMap::new();
+        sole_solution.insert(sum, 1usize);
+
+        let mut solution_set = HashSet::new();
+        solution_set.insert(sole_solution);
+
+        known_solutions.insert(sum, solution_set);
+    }
+
     for value in values.iter().cloned() {
         let less = sum - value;
 
-        if less == 0 {
-            solutions += 1;
-        }
+        if 0 < less {
+            solve(less, values, known_solutions);
 
-        else if 0 < less {
-            solutions += solve(less, values, known_solutions);
+            let mut lesser_solutions = known_solutions.get(&less).unwrap().iter().cloned().map(|mut solution| { *solution.entry(value).or_insert(0) += 1; solution} ).collect::<HashSet<_>>();
+
+            let mut target_solution = known_solutions.entry(sum).or_insert(HashSet::new());
+            for solution in lesser_solutions.into_iter() {
+                target_solution.insert(solution);
+            }
         }
     }
-
-    known_solutions.insert(sum, solutions);
-    solutions
 }
