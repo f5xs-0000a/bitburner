894620f200408956a76048a282f28086cc22d55a
F5XS <f5xs.0000a@gmail.com>
2023-06-09 00:13:36 +0800

    used classes

--- scripts/scanner.js
+++ scripts/scanner.js
@@ -1,16 +1,42 @@
+export class Machine {
+    constructor(
+        ns,
+        hostname,
+        parent_host,
+        degree
+    ) {
+        this.hostname = hostname;
+        this.parent_host = parent_host;
+        this.degree = degree;
+
+        let stats = ns.getServer(hostname);
+
+        this.is_root = stats.hasAdminRights;
+        this.backdoored = stats.backdoorInstalled;
+        this.max_money = stats.moneyMax;
+        this.player_owned = stats.purchasedByPlayer;
+        this.hacking_skill = stats.requiredHackingSkill;
+
+        // to be filled later.
+        this.path = "";
+    }
+}
+
 export function get_network(ns) {
     let traversed = [];
-    let pending = [{ "hostname": "home", "parent": "", "degree": 0 }];
+    let pending = [new Machine(ns, "home", "", 0)];
 
     while (0 < pending.length) {
-        let machine = pending.pop();
+        // BFS, not DFS. therefore, don't use pop()
+        let machine = pending.shift();
 
         // identify the children nodes of this machine
-        for (let child of ns.scan(machine["hostname"])) {
+        for (let child of ns.scan(machine.hostname)) {
             // don't bother with a child node already traversed
             let found_already = false;
             for (let traversed_child of traversed) {
-                if (traversed_child["hostname"] == child) {
+                ns.tprint(traversed_child.hostname, " ",  child)
+                if (traversed_child.hostname == child) {
                     found_already = true;
                     break;
                 }
@@ -19,51 +45,39 @@ export function get_network(ns) {
             if (found_already) {
                 continue;
             }
+            ns.tprint("NEW! " + child)
 
             // determine the properties of this new child
-            let new_child = {
-                "hostname": child,
-                "parent": machine["hostname"],
-                "degree": machine["degree"] + 1,
-            };
+            let new_child = new Machine(
+                ns,
+                child,
+                machine.hostname,
+                machine.degree + 1
+            );
 
             pending.push(new_child);
         }
-
-        let stats = ns.getServer(machine["hostname"]);
-
-        machine["is_root"] = stats.hasAdminRights;
-        machine["backdoored"] = stats.backdoorInstalled;
-        machine["max_money"] = stats.moneyMax;
-        machine["player_owned"] = stats.purchasedByPlayer;
-        machine["hacking_skill"] = stats.requiredHackingSkill;
 
         // put this node into list of traversed machines
         traversed.push(machine);
     }
 
     // adjust the traversal tree
-    let max_degree = 0;
-    for (let node of traversed) {
-        if (max_degree < node["degree"]) {
-            max_degree = node["degree"];
-        }
-    }
-
+    let max_degree = Math.max(...traversed.map(m => m.degree));
     for (let i = 0; i <= max_degree; i += 1) {
         for (let node of traversed) {
-            if (node["degree"] != i) {
+            if (node.degree != i) {
                 continue;
             }
 
             if (i == 0) {
-                node["path"] = "/" + node["hostname"];
+                node.path = "/" + node.hostname;
             }
 
             else {
-                node["path"] = traversed
-                    .find(function(n) { return n["hostname"] == node["parent"] })
-                    ["path"] + "/" + node["hostname"];
+                node.path = traversed
+                    .find(function(n) { return n.hostname == node.parent_host })
+                    .path + "/" + node.hostname;
             }
         }
     }
@@ -72,45 +86,44 @@ export function get_network(ns) {
 }
 
 export async function main(ns) {
-    let max_str_len = 0;
+    let flags = ns.flags([
+        ["path", false],
+        ["json", false],
+    ]);
+
     let network = get_network(ns);
 
     // get the string length
+    let max_str_len = 0;
     for (let machine of network) {
-        if (ns.args.includes("--path")) {
-            if (max_str_len < machine["path"].length) {
-                max_str_len = machine["path"].length;
+        if (flags["path"]) {
+            if (max_str_len < machine.path.length) {
+                max_str_len = machine.path.length;
             }
         }
 
         else {
-            if (max_str_len < machine["hostname"].length) {
-                max_str_len = machine["hostname"].length;
+            if (max_str_len < machine.hostname.length) {
+                max_str_len = machine.hostname.length;
             }
         }
     }
 
-    if (ns.args.includes("--json")) {
-        ns.print(network);
-    }
+    for (let machine of network) {
+        if (ns.args.includes("--path")) {
+            ns.tprint(
+                machine.path +
+                " ".repeat(max_str_len + 2 - machine.path.length) +
+                machine.is_root
+            );
+        }
 
-    else {
-        for (let machine of network) {
-            if (ns.args.includes("--path")) {
-                ns.tprint(
-                    machine["path"] +
-                    " ".repeat(max_str_len + 2 - machine["path"].length) +
-                    machine["is_root"]
-                );
-            }
-
-            else {
-                ns.tprint(
-                    machine["hostname"] +
-                    " ".repeat(max_str_len + 2 - machine["hostname"].length) +
-                    machine["is_root"]
-                );
-            }
+        else {
+            ns.tprint(
+                machine.hostname +
+                " ".repeat(max_str_len + 2 - machine.hostname.length) +
+                machine.is_root
+            );
         }
     }
 }
