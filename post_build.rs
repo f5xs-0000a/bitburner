c4f8e104d9c345be96d79681e4bdba78df7e9618
F5XS <f5xs.0000a@gmail.com>
2023-07-02 14:28:19 +0800

    copied post_build.rs from bitoxide

--- post_build.rs
+++ post_build.rs
@@ -17,14 +17,7 @@ use std::{
 
 use base64::write::EncoderWriter;
 use toml::Value;
-use tungstenite::{
-    handshake::server::{
-        ErrorResponse,
-        Request,
-        Response,
-    },
-    protocol::Message,
-};
+use tungstenite::protocol::Message;
 
 fn get_crate_name() -> String {
     let crate_manifest_path = var("CRATE_MANIFEST_PATH")
@@ -63,89 +56,91 @@ fn get_wasm_path(
     path
 }
 
-struct Writable {
-    contents: Vec<u8>,
-    line_len: usize,
-}
+/// Creates the WASM file, encodes it into base64, and creates a JavaScript
+/// declaration of the contents of the encoded WASM file.
+fn encode_wasm_js_decl(
+    wasm_path: &Path,
+    wasm_output: &Path,
+    crate_name: &str,
+    keep_debug: bool,
+) -> String {
+    struct Writable {
+        contents: Vec<u8>,
+        line_len: usize,
+    }
 
-impl Writable {
-    fn new() -> Writable {
-        let contents = "const wasm_b64 = \"".to_owned().into_bytes();
-        let line_len = contents.len();
+    impl Writable {
+        fn new() -> Writable {
+            let contents = "const wasm_b64 = \"".to_owned().into_bytes();
+            let line_len = contents.len();
 
-        Writable {
-            contents,
-            line_len,
+            Writable {
+                contents,
+                line_len,
+            }
+        }
+
+        fn finish(mut self) -> String {
+            self.contents.push(b'\"');
+            self.contents.push(b';');
+            return String::from_utf8(self.contents).unwrap();
         }
     }
 
-    fn finish(mut self) -> String {
-        self.contents.push(b'\"');
-        self.contents.push(b';');
-        return String::from_utf8(self.contents).unwrap();
-    }
-}
+    impl Write for Writable {
+        fn write(
+            &mut self,
+            buf: &[u8],
+        ) -> IoResult<usize> {
+            let mut idx = 0;
 
-impl Write for Writable {
-    fn write(
-        &mut self,
-        buf: &[u8],
-    ) -> IoResult<usize> {
-        let mut idx = 0;
+            while idx < buf.len() {
+                let chars_left_on_line = 79 - self.line_len;
 
-        while idx < buf.len() {
-            let chars_left_on_line = 79 - self.line_len;
+                if chars_left_on_line == 0 {
+                    self.contents.push(b'\\');
+                    self.contents.push(b'\n');
+                    self.line_len = 0;
+                    continue;
+                }
 
-            if chars_left_on_line == 0 {
-                self.contents.push(b'\\');
-                self.contents.push(b'\n');
-                self.line_len = 0;
-                continue;
+                let chars_left_on_buffer = buf.len() - idx;
+
+                let chars_to_write =
+                    chars_left_on_line.min(chars_left_on_buffer);
+
+                self.contents
+                    .write(&buf[idx .. idx + chars_to_write])
+                    .unwrap();
+                idx += chars_to_write;
+                self.line_len += chars_to_write;
             }
 
-            let chars_left_on_buffer = buf.len() - idx;
-
-            let chars_to_write = chars_left_on_line.min(chars_left_on_buffer);
-
-            self.contents.write(&buf[idx .. idx + chars_to_write]);
-            idx += chars_to_write;
-            self.line_len += chars_to_write;
+            Ok(buf.len())
         }
 
-        Ok(buf.len())
+        fn flush(&mut self) -> IoResult<()> {
+            Ok(())
+        }
     }
-
-    fn flush(&mut self) -> IoResult<()> {
-        Ok(())
-    }
-}
-
-fn main() {
-    let crate_target_dir = var("CRATE_TARGET_DIR")
-        .map(|var| PathBuf::from(var))
-        .expect("Cannot read CRATE_TARGET_DIR environment variable");
-    let profile = var("CRATE_PROFILE")
-        .expect("Cannot read CRATE_PROFILE environment variable");
-    let target_triple = var("CRATE_TARGET_TRIPLE")
-        .expect("Cannot read CRATE_TARGET_TRIPLE environment variable");
-
-    let crate_name = get_crate_name();
-    let wasm_path =
-        get_wasm_path(&crate_name, &crate_target_dir, &profile, &target_triple);
-
-    let wasm_output = crate_target_dir.join("wasm_output");
 
     // run wasm-bindgen
-    Command::new("wasm-bindgen")
+    let mut command = Command::new("wasm-bindgen");
+    command
         .arg("--target")
         .arg("web")
+        .arg("--no-typescript")
         .arg(&wasm_path)
         .arg("--out-dir")
-        .arg(&wasm_output)
-        .output()
-        .expect("Cannot run wasm-bindgen");
+        .arg(&wasm_output);
 
-    let mut wasm_file = OpenOptions::new()
+    if keep_debug {
+        command.arg("--keep-debug");
+    }
+
+    command.output().expect("Cannot run wasm-bindgen");
+
+    let wasm_file = OpenOptions::new()
         .read(true)
         .open(wasm_output.join(format!("{}_bg.wasm", crate_name)))
         .map(|file| BufReader::new(file))
@@ -160,17 +155,20 @@ fn main() {
         );
 
         for byte in wasm_file.bytes() {
-            encoder.write_all(&[byte.unwrap()]);
+            encoder.write_all(&[byte.unwrap()]).unwrap();
         }
-        encoder.flush();
+        encoder.flush().unwrap();
         encoder.finish().unwrap();
     }
 
-    let mut resulting_javascript = writable.finish();
-    resulting_javascript += "\n\n";
+    writable.finish()
+}
 
-    // read the contents of the javascript file
-
+fn join_with_binder(
+    js_str: &mut String,
+    wasm_output: &Path,
+    crate_name: &str,
+) {
     let mut wasm_file = OpenOptions::new()
         .read(true)
         .open(wasm_output.join(format!("{}.js", crate_name)))
@@ -193,19 +191,61 @@ fn main() {
             break;
         }
 
-        resulting_javascript += &buffer;
+        *js_str += &buffer;
     }
 
-    resulting_javascript += include_str!("./addendum.js");
+    *js_str += include_str!("./addendum.js");
+}
 
+fn main() {
+    let crate_target_dir = var("CRATE_TARGET_DIR")
+        .map(|var| PathBuf::from(var))
+        .expect("Cannot read CRATE_TARGET_DIR environment variable");
+    let profile = var("CRATE_PROFILE")
+        .expect("Cannot read CRATE_PROFILE environment variable");
+    let target_triple = var("CRATE_TARGET_TRIPLE")
+        .expect("Cannot read CRATE_TARGET_TRIPLE environment variable");
+
+    let crate_name = get_crate_name();
+    let wasm_path =
+        get_wasm_path(&crate_name, &crate_target_dir, &profile, &target_triple);
+
+    let wasm_output = crate_target_dir.join("wasm_output");
+
+    // read the contents of the javascript file
+    let mut wasm_b64 = encode_wasm_js_decl(
+        &*wasm_path,
+        &*wasm_output,
+        &*crate_name,
+        var("DEBUG").as_deref() == Ok("true"),
+    );
+
+    // join the contents of wasm_b64 with the binder output from the bindgen
+    join_with_binder(&mut wasm_b64, &*wasm_output, &*crate_name);
+
+    if var("OUTPUT_MODE").as_deref() == Ok("stdout") {
+        println!("{}", wasm_b64);
+        eprintln!("Written to stdout. Please copy the output.");
+    }
+    else {
+        post_to_websocket(&*wasm_b64, 7953, &*crate_name);
+    }
+}
+
+fn post_to_websocket(
+    contents: &str,
+    port_number: u16,
+    crate_name: &str,
+) {
     // start the websocket server
-    let server = std::net::TcpListener::bind("127.0.0.1:7953").unwrap();
+    let server =
+        std::net::TcpListener::bind(&*format!("127.0.0.1:{}", port_number))
+            .unwrap();
 
     eprintln!("Listening on port 7953...");
 
-    let server = server.incoming().next().unwrap();
-    let mut websocket =
-        tungstenite::accept_hdr(server.unwrap(), callback).unwrap();
+    let stream = server.incoming().next().unwrap();
+    let mut websocket = tungstenite::accept(stream.unwrap()).unwrap();
 
     eprintln!("Listener found. Uploading...");
     let message = serde_json::json!({
@@ -214,7 +254,7 @@ fn main() {
         "method": "pushFile",
         "params": {
             "filename": format!("{}.js", crate_name),
-            "content": resulting_javascript,
+            "content": contents,
             "server": "home",
         }
     })
@@ -222,19 +262,5 @@ fn main() {
 
     websocket.write_message(Message::Text(message)).unwrap();
 
-    websocket.close(None);
-}
-
-fn callback(
-    req: &Request,
-    mut response: Response,
-) -> Result<Response, ErrorResponse> {
-    for (ref header, _value) in req.headers() {
-        println!("* {}", header);
-    }
-
-    // Let's add an additional header to our response to the client.
-    let headers = response.headers_mut();
-
-    Ok(response)
+    websocket.close(None).unwrap();
 }
