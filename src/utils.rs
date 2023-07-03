7e7e345e029ca59b54b65ec99dcffc653b8d4c0d
F5XS <f5xs.0000a@gmail.com>
2023-07-03 14:08:01 +0800

    add convenience definitions to rational multiplication

--- src/utils.rs
+++ src/utils.rs
@@ -8,3 +8,53 @@ pub fn get_attribute<T>(
 ) -> Result<Option<T>, JsValue> {
     Reflect::get(object, &JsValue::from_str(field_name)).map(|x| mapper(&x))
 }
+
+/// Performs x * p in a convoluted way that reduces errors.
+pub fn rational_mult_usize(
+    x: usize,
+    p: f64,
+) -> usize {
+    if x == 0 {
+        return 0;
+    }
+
+    // find the optimal power of 2 that will make x * p no greater than
+    // usize::MAX so it does not overflow. it's basically just:
+    // log(MAX) - log(x) - max(0, log(p))
+    let power_2 =
+        (usize::MAX.ilog2() - x.ilog2() - (p.log2().max(0.) as u32)) as i32;
+
+    // x * p = x * (p * q) / q
+    // calculate q, which is the power of 2
+    let q = 2f64.powi(power_2);
+
+    // calculate (p * q)
+    let p_uint = (p * q) as usize;
+
+    x * p_uint / (q as usize)
+}
+
+/// Performs x * p in a convoluted way that reduces errors.
+pub fn rational_mult_u64(
+    x: u64,
+    p: f64,
+) -> u64 {
+    if x == 0 {
+        return 0;
+    }
+
+    // find the optimal power of 2 that will make x * p no greater than
+    // usize::MAX so it does not overflow. it's basically just:
+    // log(MAX) - log(x) - max(0, log(p))
+    let power_2 =
+        (usize::MAX.ilog2() - x.ilog2() - (p.log2().max(0.) as u32)) as i32;
+
+    // x * p = x * (p * q) / q
+    // calculate q, which is the power of 2
+    let q = 2f64.powi(power_2);
+
+    // calculate (p * q)
+    let p_uint = (p * q) as u64;
+
+    x * p_uint / (q as u64)
+}
