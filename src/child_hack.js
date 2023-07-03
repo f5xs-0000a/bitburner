004d9f79f3fde2076f6bb069dae88e93c2a03d1a
F5XS <f5xs.0000a@gmail.com>
2023-07-03 14:12:25 +0800

    add parameter to wait on child scripts

--- src/child_hack.js
+++ src/child_hack.js
@@ -1,5 +1,11 @@
 /** @param {NS} ns */
 export async function main(ns) {
     let flags = ns.flags([]);
+    let sleep_time = Number(flags["_"][1]);
+
+    if (0 < sleep_time) {
+        await ns.sleep(sleep_time);
+    }
+
     await ns.hack(flags["_"][0]);
 }
