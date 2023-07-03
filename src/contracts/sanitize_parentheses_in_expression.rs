370b29c4dd50b1c77ec36b92974330d4808fe89f
F5XS <f5xs.0000a@gmail.com>
2023-07-03 14:12:42 +0800

    more unfinished thing for parentheses contract

--- src/contracts/sanitize_parentheses_in_expression.rs
+++ src/contracts/sanitize_parentheses_in_expression.rs
@@ -7,77 +7,86 @@
 //! the result should be an array with only an empty string.
 
 fn solve(input: &str) -> Vec<String> {
-    // an interesting way to rephrase this is considering that opening braces
-    // are +1 and closing braces are -1. for each of the parentheses, add their
-    // corresponding values into the sum. plot the sum along with the
-    // parentheses. for example:
+    // one way to solve this is by using stacks and iterators. for every
+    // character on the string, pair it with its index and prepare to push it
+    // into the stack.
+    // stack.
+    // 
+    // if it is not a parenthesis, immediately pop it out of the stack.
+    // if it is an opening parenthesis, push it into the stack.
+    // if it is a closing parenthesis, pop from the stack.
+    //     if it is an opening parenthesis, keep both parentheses out of the
+    //         stack
+    //     if it is not, record the first index this has happened. also record
+    //         how many times this has happened
     //
-    // chars  (  (  (  )  )  )
-    // sums   1  2  3  2  1  0
+    // for the remaining opening parentheses still in the stack, record the
+    // first index it has happened and how many are remaining
     //
-    // in order for a parentheses string to be valid:
-    // 1. the sum must end in 0, and
-    // 2. the sum never goes negative
+    // for both cases, you will have the index of when it happened first/last
+    // and its occurrences.
+    // 
+    // if a closing parenthes
     //
-    // chars  (  (  (  )  )
-    // sums   1  2  3  2  1  (invalid due to not ending with 0)
-    //
-    // chars  (  )  )  (  )
-    // sums   1  0 -1  0 -1  (invalid due to the sum dipping to negatives)
-    //
-    // in case the current string is invalid, there is only one way to check if
-    // there are characters removable that can make the string valid.
-    // 1. there must be a sum anywhere that is higher than the final sum
-    // for all the sections that have dipped to negatives, it is considered
-    // invalid
-    //
-    // chars  (  )  )  (  )
-    // sums   1  0 -1  0 -1
-    //       (invalid)
+    // note: doesn't work in )()())(
 
-    let (skip_amount, sums) = loop {
-        let mut skip_amount = 0;
+    let mut cur_vec = Vec::with_capacity(input.len());
 
-        // plot the sums
-        let sums = input.chars()
-            .map(|c| match c {
-                '(' => 1,
-                ')' => -1,
-                _ => 0,
-            })
-            .skip(skip_amount)
-            .scan(0, |mut state, x| {
-                *state += x;
-                Some(*state)
-            })
-            .collect::<Vec<_>>();
+    let mut sum = 0;
+    for (idx, ch) in input.chars().enumerate() {
+        let open = match ch {
+            '(' => true,
+            ')' => false,
+            _ => continue,
+        };
 
-        let last_sum = sums.last().unwrap_or(0);
-
-        if sums.last().unwrap_or(0) < 0 {
-            // on the case that the last sum is negative, find on the sums the
-            // earlist value that is the same as the last sum
-            match sums.iter().find(|s| *s == s) {
-                // the sum isn't found. there is none.
-                None => return vec![],
-                Some(idx) => {
-                    skip_amount = idx + 1,
-                    continue;
-                },
-            };
+        if open {
+            sum += 1;
         }
 
-        break (skip_amount, sums);
-    };
+        else {
+            sum -= 1;
+        }
 
-    // if the sum is valid in the first place by having no negatives, return it
-    if sums.iter().all(|x| 0 <= x) {
-        return vec![input.to_owned()];
+        char_vec.push((open, idx, sum));
     }
 
-    // count the number of excess opens and closes
-    let excess_opens = sums.last().unwrap().max(0);
-    let excess_closes = (-sums.last().unwrap()).max(0);
+    let mut stack_counter = 0usize;
+    let mut over_close = 0;
+    let mut lowest_sum = 0;
+    let mut latest_close_index = 0;
+    let mut earliest_close_index = 0;
+
+    for (open, idx, sum) in cur_vec.iter() {
+        if open {
+            stack += 1;
+        }
+
+        else if 0 < stack_counter {
+            stack_counter -= 1
+        }
+
+        else {
+            over_close += 1;
+            latest_close_index = idx;
+        }
+
+        if sum < lowest_sum {
+            lowest_sum = sum;
+            earliest_close_index = idx;
+        }
+    }
+
+    let over_open = sum - lowest_sum;
+
+    // we've got the points of until when we're going to remove parentheses and
+    // how many. which groups of parentheses and how many of them are we going
+    // to remove per group?
+    //
+    // that's obtained by checking how many pairs already existed for a given
+    // smth
+
+
 }
 
 #[cfg(test)]
